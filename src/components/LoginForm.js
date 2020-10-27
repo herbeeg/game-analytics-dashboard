4475f70bc779eb4ae99290d0b6b708ad0ab6e498
Jon Herbst <herbtaculargames@gmail.com>
2020-10-27 17:55:14 +0000

    Starting to convert initial login landing page to centred grid with stacked input and button segment

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,58 +1,65 @@
 import React, { useState } from "react";
-import { Button, Form, Icon, Input } from "semantic-ui-react";
+import { Button, Form, Grid, Header, Icon, Input, Segment } from "semantic-ui-react";
 
 export const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   
   return (
-    <Form>
-      <Form.Field required>
-        <label>Email Address:</label>
-        <Input 
-          icon='at' 
-          iconPosition='left' 
-          placeholder='Email' 
-          value={email} 
-          onChange={e => setEmail(e.target.value)} 
-        />
-      </Form.Field>
+    <Grid verticalAlign='middle' center>
+      <Grid.Column>
+        <Header as='h2'>Game Analytics Dashboard</Header>
+        <Form size='large'>
+          <Segment stacked='true'>
+            <Form.Field required>
+              <label>Email Address:</label>
+              <Input 
+                icon='at' 
+                iconPosition='left' 
+                placeholder='Email' 
+                value={email} 
+                onChange={e => setEmail(e.target.value)} 
+              />
+            </Form.Field>
 
-      <Form.Field required>
-        <label>Password:</label>
-        <Input 
-          type='password'
-          icon='code' 
-          iconPosition='left' 
-          placeholder='Password' 
-          value={password} 
-          onChange={e => setPassword(e.target.value)} 
-        />
-      </Form.Field>
+            <Form.Field required>
+              <label>Password:</label>
+              <Input 
+                type='password'
+                icon='code' 
+                iconPosition='left' 
+                placeholder='Password' 
+                value={password} 
+                onChange={e => setPassword(e.target.value)} 
+              />
+            </Form.Field>
 
-      <Button 
-        type='submit' 
-        onClick={async () => {
-          const user = {email, password};
-          const response = await fetch('/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-          })
+            <Button 
+              type='submit' 
+              onClick={async () => {
+                const user = {email, password};
+                const response = await fetch('/login', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(user)
+                })
 
-          if (response.ok) {
-            // Authenticate the user.
-          } else {
-            // Clear the input fields
-            setEmail('')
-            setPassword('')
-          }
-        }}
-      >
-        Login
-      </Button>
-    </Form>
+                if (response.ok) {
+                  // Authenticate the user.
+                } else {
+                  // Clear the input fields
+                  setEmail('')
+                  setPassword('')
+                }
+              }}
+            >
+              Login
+            </Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
   )
 }
