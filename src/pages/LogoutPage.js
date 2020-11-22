424d61c3a9e1e33eff8c5e3985a9b332e24e4706
Jon Herbst <herbtaculargames@gmail.com>
2020-11-22 16:32:53 +0000

    Remove access token from session storage when users hit the /logout page

--- src/pages/LogoutPage.js
+++ src/pages/LogoutPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 export default function LogoutPage() {
+  sessionStorage.removeItem('access_token')
+
   return (
     <Redirect to='/login' />
   );
